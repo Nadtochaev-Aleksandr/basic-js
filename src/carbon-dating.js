00d07bb059dbd3e060d9ff7b1ee5a0860333dcfa
Aleksandr <Nadtochaev_aleksandr@mail.ru>
2025-01-04 09:56:45 +0300

    feat: carbone-dating done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,29 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity === '' || sampleActivity === ' ') {
+    // console.log(`Вводимый параметр должен быть типа "string" и не быть пустым - выводим false`)
+    return false;
+  }
+  // console.log(`введенное число ${sampleActivity} - активность исследуемого образца`);
+  const numberSampleActivity = Number(sampleActivity);
+  // console.log(numberSampleActivity);
+  // console.log(typeof numberSampleActivity);
+  // console.log(`результат конвертации введенного числа к number - ${numberSampleActivity}`);
+  if (typeof numberSampleActivity !== 'number' || Number.isNaN(numberSampleActivity) || numberSampleActivity <= 0) {
+    // console.log(`Так как при конвертации введенного значения не число либо неадекватное значение - выводим false`)
+    return false;
+  }
+  const constSpeedReacction = 0.693 / HALF_LIFE_PERIOD;
+  // console.log(`константа скорости реакции распада равна ${constSpeedReacction} год в степени минус 1`);
+  const resultOld = Math.log(MODERN_ACTIVITY / sampleActivity) / constSpeedReacction;
+  // console.log(`Итого возраст образца составляет ${resultOld}`);
+  // console.log(`Округление полученного значения до большего ${Math.ceil(resultOld)} лет`);
+  if (resultOld < 0) {
+    return false;
+  }
+  return Math.ceil(resultOld);
 }
 
 module.exports = {
