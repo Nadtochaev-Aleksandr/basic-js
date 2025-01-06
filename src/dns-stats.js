ebacdfb158a025acd5233362ada237fbeafa55b7
Aleksandr <Nadtochaev_aleksandr@mail.ru>
2025-01-06 23:27:17 +0300

    feat: dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  if (!Array.isArray(domains)) {
+    throw new NotImplementedError('В качестве аргумента передан не массив данных');
+  }
+  let resultObject = {};
+  let iterationObject = {};
+  let domenContainsNumber = 0;
+  let addresContainsNumber = 0;
+  let indexContainsNumber = 0;
+
+  for (let i = 0; i < domains.length; i += 1) {
+    console.log(`Рассматриваемый элемент domains[${i}] = ${domains[i]}`);
+    let domensElements = domains[i].split('.');
+    let domen = '';
+    let addres = '';
+    let index = '';
+    console.log(`промежуточный элемент domensElements = ${domensElements}`);
+    domen = domen + '.' + domensElements[domensElements.length - 1];
+    console.log(`domen = ${domen}`);
+    addres = domensElements[domensElements.length - 2];
+    console.log(`addres = ${addres}`);
+    if (domains[i].includes(domen)) {
+      domenContainsNumber += 1;
+      console.log(`domenContainsNumber = ${domenContainsNumber}`)
+    }
+    if (domains[i].includes(addres)) {
+      addresContainsNumber += 1;
+      console.log(`addresContainsNumber = ${addresContainsNumber}`)
+    }
+    Object.assign(iterationObject, {domen: domenContainsNumber});
+    Object.assign(iterationObject, {addres: addresContainsNumber});
+    if (domensElements.length > 2) {
+      index = domensElements[domensElements.length - 3];
+      if (domains[i].includes(index)) {
+        indexContainsNumber += 1;
+        console.log(`indexContainsNumber = ${indexContainsNumber}`)
+      }
+      console.log(`index = ${index}`);
+      Object.assign(iterationObject, {index: indexContainsNumber});
+    }
+    console.log(iterationObject);
+  }
+  Object.assign(resultObject, iterationObject);
+  console.log(resultObject);
+  return resultObject;
 }
 
 module.exports = {
